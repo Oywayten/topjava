60c83e5eeb3a19c831571832ae95448be565c80d
oywayten <cpc1251@mail.ru>
2023-01-29 16:42:19 +0300

    delet UserMealsUtil#filteredByStreams2 and UserMealsUtil#filteredByStreams3

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -8,11 +8,6 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.util.*;
-import java.util.function.BiConsumer;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 /**
@@ -127,69 +122,5 @@ public class UserMealsUtil {
      */
     private static UserMealWithExcess getUserMealWithExcess(UserMeal userMeal, UserMealWithExcess.Excess excess) {
         return new UserMealWithExcess(userMeal.getDateTime(), userMeal.getDescription(), userMeal.getCalories(), excess);
-    }
-
-    /**
-     * Optional2 by Stream API.
-     * The method receives a list of meals to check whether the specified calorie intake was exceeded in the specified period.
-     * The method is implemented on the Stream API.
-     *
-     * @param meals          food list {@link UserMeal}
-     * @param startTime      the start of the daily time interval.
-     * @param endTime        end of the daily time slot.
-     * @param caloriesPerDay int the maximum number of calories for the interval.
-     * @return {@link UserMealWithExcess} a list of entries with a calorie surplus flag.
-     * калорий.
-     */
-    public static List<UserMealWithExcess> filteredByStream2(
-            List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        Map<LocalDate, UserMealWithExcess.Excess> dateExcessMap = new HashMap<>();
-        return meals.stream().
-                peek(meal -> dateExcessMap.merge(meal.getDate(), new UserMealWithExcess.Excess(caloriesPerDay, meal.getCalories()),
-                        (excess, excess2) -> {
-                            excess.addCalories(meal.getCalories());
-                            return excess;
-                        }
-                ))
-                .filter(userMeal -> TimeUtil.isBetweenHalfOpen(userMeal.getTime(), startTime, endTime))
-                .map(meal -> getUserMealWithExcess(meal, dateExcessMap.get(meal.getDate()))).collect(Collectors.toList());
-    }
-
-    /**
-     * Optional2 by Stream API with custom Collector.
-     * The method receives a list of meals to check whether the specified calorie intake was exceeded in the specified period.
-     * The method is implemented on the Stream API.
-     *
-     * @param meals          food list {@link UserMeal}
-     * @param startTime      the start of the daily time interval.
-     * @param endTime        end of the daily time slot.
-     * @param caloriesPerDay int the maximum number of calories for the interval.
-     * @return {@link UserMealWithExcess} a list of entries with a calorie surplus flag.
-     * калорий.
-     */
-    public static List<UserMealWithExcess> filteredByStream3(
-            List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        return meals.stream().collect(Collector.of(HashMap::new, (map, meal) ->
-                map.merge(meal.getDate(), new ArrayList<UserMealWithExcess>() {{
-                    add(getUserMealWithExcess(meal, new UserMealWithExcess.Excess(caloriesPerDay, meal.getCalories())));
-                }}, (userMealWithExcesses, userMealWithExcesses1) -> {
-                    final UserMealWithExcess.Excess excess = userMealWithExcesses.get(0).getExcess();
-                    userMealWithExcesses1.get(0).setExcess(excess);
-                    userMealWithExcesses.addAll(userMealWithExcesses1);
-                    return userMealWithExcesses;
-                }), (map1, map2) -> {
-                    map1.forEach(
-                            (date, userMealWithExcesses21) -> map2.merge(date, userMealWithExcesses21, ((userMealWithExcesses11, userMealWithExcesses2) -> {
-                                userMealWithExcesses11.addAll(userMealWithExcesses2);
-                                return userMealWithExcesses11;
-                            }))
-                    );
-                    map1.putAll(map2);
-                    return map1;
-                }, (Function<Map<LocalDate, List<UserMealWithExcess>>, List<UserMealWithExcess>>) (map3) ->
-                        map3.entrySet().stream()
-                                .flatMap(entry -> entry.getValue().stream())
-                                .filter(excess1 -> TimeUtil.isBetweenHalfOpen(excess1.getDateTime().toLocalTime(), startTime, endTime))
-                                .collect(Collectors.toList())));
     }
 }
